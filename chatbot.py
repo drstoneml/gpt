cdfe7862b578f4f4f795fcc51ecd9b193414aaed
drstone.ml <manavchavda594@gmail.com>
2025-07-24 07:05:32 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,55 @@
+"""
+Install the Google AI Python SDK
+
+$ pip install google-generativeai
+
+See the getting started guide for more information:
+https://ai.google.dev/gemini-api/docs/get-started/python
+"""
+
+import google.generativeai as genai
+
+
+
+
+genai.configure(api_key="AIzaSyAN86LnOBxczIBfoRvuysx13w_SwcxznqY")
+
+# Create the model
+# See https://ai.google.dev/api/python/google/generativeai/GenerativeModel
+generation_config = {
+  "temperature": 0.5,
+  "top_p": 0.95,
+  "top_k": 64,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-pro",
+  generation_config=generation_config,
+  system_instruction="You are a assistant chatbot. You are here to help the user with their queries.",
+)
+
+
+
+chat_session = model.start_chat(
+    history=[]
+)
+
+print("Bot: Hello, how can I help you?")
+print()
+
+while True:
+
+    user_input = input("You: ")
+    print()
+
+    response = chat_session.send_message(user_input)
+
+    model_response = response.text
+
+    print(f'Bot: {model_response}')
+    print()
+
+    chat_session.history.append({"role": "user", "parts": [user_input]})
+    chat_session.history.append({"role": "model", "parts": [model_response]})
